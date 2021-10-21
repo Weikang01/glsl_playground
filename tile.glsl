b7ba215e40a5996eb81b7bb32757a7d87f994969
Weikang01 <lwkdota@qq.com>
2021-10-21 00:39:54 -0400

    first commit

---
+++ tile.glsl
@@ -0,0 +1,41 @@
+#define S(v, p) smoothstep(v+0.005, v, p)
+#define PI 3.14159265359
+#define TWO_PI 6.28318530718
+
+uniform vec3      iResolution;           // viewport resolution (in pixels)
+uniform float     iTime;                 // shader playback time (in seconds)
+uniform float     iTimeDelta;            // render time (in seconds)
+uniform int       iFrame;                // shader playback frame
+uniform float     iChannelTime[4];       // channel playback time (in seconds)
+uniform vec3      iChannelResolution[4]; // channel resolution (in pixels)
+uniform vec4      iMouse;                // mouse pixel coords. xy: current (if MLB down), zw: click
+// uniform samplerXX iChannel0..3;          // input channel. XX = 2D/Cube
+uniform vec4      iDate;                 // (year, month, day, time in seconds)
+uniform float     iSampleRate;           // sound sample rate (i.e., 44100)
+
+float box(in vec2 uv, in vec2 size)
+{
+    float b = S(size.x, uv.x);
+    b *= S(size.x, -uv.x);
+    b *= S(size.y, uv.y);
+    return b*S(size.y, -uv.y);
+}
+
+mat2 rotate(in float angle)
+{
+    return mat2(cos(angle), -sin(angle),
+                sin(angle), cos(angle));
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 uv = (fragCoord-.5*iResolution.xy)/iResolution.y;
+    vec2 mouse = (iMouse.xy-.5*iResolution.xy)/iResolution.y;
+    uv *= 3.;
+    uv = fract(uv);
+    
+    vec3 col = vec3(0.);
+    col += box(rotate(.25 * PI) * (uv-vec2(.5))*1.43, vec2(.5));
+    
+    fragColor = vec4(col,1.0);
+}
